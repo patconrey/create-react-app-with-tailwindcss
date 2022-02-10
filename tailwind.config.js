f8f8f716d13e733e4e59531c2835df0f6eafa1af
Patrick Conrey <patrickconrey@gmail.com>
2022-02-10 11:01:32 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
